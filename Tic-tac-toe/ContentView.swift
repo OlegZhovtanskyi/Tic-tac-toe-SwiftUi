84b7dd13140aaafd371277f4ae12c5891e446877
Oleg Zhovtanskyi <o.zhovtanskyi@gmail.com>
2023-11-12 17:33:41 +0100

    Initial Commit

---
+++ Tic-tac-toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic-tac-toe
+//
+//  Created by Oleg Zhovtanskyi on 12/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic-tac-toe/Tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_tac_toeApp.swift
+//  Tic-tac-toe
+//
+//  Created by Oleg Zhovtanskyi on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
